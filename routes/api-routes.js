ab3a1441d583375a6a10853ab80e36b08ede12ca
Aileen Flanagan <aileenmflanagan@hotmail.com>
2019-03-12 11:50:15 -0500

    added some api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,30 +2,52 @@
 
 // var express = require("express");
 // var router = express.Router();
-var db = require("../models"); 
+var db = require("../models");
 
 
 //..........................................................
 
-module.exports= function(app){
-//GET route 'findAll' to get all chores from database.
-app.get("/api/household", function(req, res) {
-    db.chores.findAll({})
-        .then(function(dbChores){
-            res.json(dbChores);
+module.exports = function (app) {
+    //GET route 'findAll' to get all chores from database.
+    app.get("/api/household", function (req, res) {
+        db.chores.findAll({})
+            .then(function (dbChores) {
+                res.json(dbChores);
+            })
+    });
+
+
+
+
+    //Create a chore
+    app.post("/api/chore", function (req, res) {
+        console.log(req.body);
+        db.chores.create({
+            //need to make sure this matches
+            chore_name: req.body.chore
         })
-  });
-  
+            .then(function (dbChore) {
+                res.json(dbChore);
+            });
+    });
+
+    //Assign Chore
+    app.put("/api/chore/assign", function (req, res) {
+        //not sure this is right
+        db.chores.update(req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            }
+            )
+    });
 
 
+    //Update Chore Status (complete)
 
-//Create a chore
 
-
-//Update a chore
-
-  
-//Delete a chore
+    //Delete a chore
 
 }
 
