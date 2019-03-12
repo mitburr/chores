e794ba212abc4f5f8301c84d9ae9f19d5d1994f6
mitburr <mitburr@gmail.com>
2019-03-12 12:00:50 -0500

    work on the index for authentication

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,9 @@
+const router        = require('express').Router()
+const jwt           = require('jsonwebtoken');
+const db            = require('../models')
+const {JWT_OPTIONS, JWT_SECRET_KEY, TEST_USER}            = require('../config/jwt')
 
-
-// var express = require("express");
-// var router = express.Router();
-var db = require("../models"); 
-
-
+//setup requires
 //..........................................................
 
 module.exports= function(app){
@@ -14,7 +13,7 @@ app.get("/api/household", function(req, res) {
         .then(function(dbChores){
             res.json(dbChores);
         })
-  });
+  })};
   
 
 
@@ -27,7 +26,38 @@ app.get("/api/household", function(req, res) {
   
 //Delete a chore
 
-}
+
+//post route which connects to the login button.
+router.post('/token', function(req,res) {
+    //Normally, you would fetch the user from the database here.
+    //then save the relevant info in user details
+    const userDetails = {
+        username: TEST_USER.username,
+        id: TEST_USER.id,
+        foo: TEST_USER.foo,
+    };
+
+
+    // do some sort of check that the user/password is correct:
+    //normally, you would do this with the user in the database. 
+    //in this case, we just have a "test user" saved in a file somewhere
+    if (req.body.username === TEST_USER.username && req.body.password === TEST_USER.password) {
+
+        jwt.sign(userDetails, JWT_SECRET_KEY, JWT_OPTIONS, 
+        function(err, token) {
+            if (err) return res.sendStatus(500).json(err) //do some error checking
+            res.json({
+                user: userDetails,
+                token: token,
+            })
+
+        })
+
+    }
+    else {
+        res.sendStatus(401).send();
+    }
+});
 
 // Export routes for server.js to use.......................
 module.exports = router;
