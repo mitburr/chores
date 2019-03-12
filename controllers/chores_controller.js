748313084d333ea8ddaada9afc6a5092dea84440
Aileen Flanagan <aileenmflanagan@hotmail.com>
2019-03-12 10:50:52 -0500

    commented out the controller.js

--- controllers/chores_controller.js
+++ controllers/chores_controller.js
@@ -1,27 +1,29 @@
-var express = require("express");
-var router = express.Router();
-var models = require("../models"); 
+//USING ROUTES FOLDER (api-routes.js or html-routes.js)
+
+// var express = require("express");
+// var router = express.Router();
+// var models = require("../models"); 
 
 
-//..........................................................
+// //..........................................................
 
 
-//GET route 'findAll' to get all chores from database.
-router.get("/", function(req, res) {
-    res.redirect("/index");
-  });
+// //GET route 'findAll' to get all chores from database.
+// router.get("/", function(req, res) {
+//     res.redirect("/index");
+//   });
   
-  //GET route 'findAll' to get all burgers from database.
-  router.get('/index', function(req, res){
-    models.chores.findAll({})
-    .then(function(data) {
-      var hbsObject = 
-      {
-        chores: data
-      }; 
-      res.json(hbsObject); 
-    });
-  });
+//   //GET route 'findAll' to get all burgers from database.
+//   router.get('/index', function(req, res){
+//     models.chores.findAll({})
+//     .then(function(data) {
+//       var hbsObject = 
+//       {
+//         chores: data
+//       }; 
+//       res.json(hbsObject); 
+//     });
+//   });
 //Create a chore
 
 
