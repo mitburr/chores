d0d4c12860a92d595e851ba355b82eaffa31e38f
rtonn <rtonn09umn@gmail.com>
2019-03-13 14:05:54 -0500

    db houseId

--- db/schema_jaws.sql
+++ db/schema_jaws.sql
@@ -19,42 +19,28 @@ id INTEGER NOT NULL AUTO_INCREMENT,
 person_name VARCHAR(255) NOT NULL, 
 person_email VARCHAR (255), 
 isParent BOOLEAN DEFAULT false,
-userID VARCHAR(255),
+userID VARCHAR(255) Unique,
 password VARCHAR (255),
 createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-houseId VARCHAR(255) NOT NULL,
+HouseId VARCHAR(255) NOT NULL,
 PRIMARY KEY (id)
 ); 
 
-Use chores_db; 
-CREATE TABLE houses (
-id INTEGER NOT NULL AUTO_INCREMENT,
-house_name VARCHAR(255) NOT NULL, 
-isActive BOOLEAN DEFAULT false,
-createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
-); 
 
 
 -----------------------------------
-USE chores_db;
-INSERT into houses (house_name, isActive)
-VALUES ("The Millers", 1),
-	   ("The Smiths", 0 );
        
 USE chores_db;        
-INSERT into people (person_name, person_email, isParent, houseId)
-
-VALUES ("Mom Miller", "mom@email.com", 1, 1), 
-	   ("Dad Miller", "dad@email.com", 1, 1),  
-       ("Billy Miller", "son@email.com", 0,1),
-       ("Becky Miller", "daughter@email.com", 0, 1),
-	   ("Mr Smith", "msmith@email.com", 1, 2), 
-	   ("Mrs Smith", "mrssmith@email.com", 1, 2),  
-       ("Jonny Smith", "jonnysmith@email.com", 0,2),
-       ("Sis Smith", "sissmith@email.com", 0, 2); 
+INSERT into people (person_name, person_email, isParent, userID, password, houseId)
+VALUES ("Mom Miller", "mom@email.com", 1, "mmiller1", "password1", 1), 
+	   ("Dad Miller", "dad@email.com", 1,  'dmiller1', 'password2',1),  
+       ("Billy Miller", "son@email.com", 0, 'bmiller1', 'password3',1),
+       ("Becky Miller", "daughter@email.com", 0, 'beckmiller1', 'password4', 1),
+	   ("Mr Smith", "msmith@email.com", 1, 'mr_smith1', 'password5', 2), 
+	   ("Mrs Smith", "mrssmith@email.com", 1, 'momsmith1', 'password6', 2),  
+       ("Jonny Smith", "jonnysmith@email.com",0, 'jsmith1', 'password7', 2),
+       ("Sis Smith", "sissmith@email.com", 0, 'sissmith1', 'password8', 2); 
 
 USE chores_db; 
 INSERT into chores (chore_name, chore_importance, chore_complete, personId)
@@ -62,11 +48,8 @@ VALUES ("Dishes", "High", false, 1),
 	   ("Laundry", "Medium", true, 2),
 	   ("Mow the Lawn", "Low", false, 3),
 	   ("Feed the Dog", "Medium", true, 4); 
-
-
+	   
 --------------------------------------
- Use chores_db; 
-select * from houses
 
  Use chores_db; 
 select * from people
