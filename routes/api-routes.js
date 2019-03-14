a4646f60f31ba3b36bc18505538145aec7065e24
mitburr <mitburr@gmail.com>
2019-03-14 11:53:05 -0500

    authetnication integration completed on separate doc

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
 const router        = require('express').Router()
 const jwt           = require('jsonwebtoken');
 const db            = require('../models')
+const cookieparser = require('cookie-parser');
+
 const {JWT_OPTIONS, JWT_SECRET_KEY, TEST_USER}            = require('../config/jwt')
 
 //setup requires
@@ -9,6 +11,8 @@ const {JWT_OPTIONS, JWT_SECRET_KEY, TEST_USER}            = require('../config/j
 
 //GET ROUTES----------------------------------------------
 module.exports= function(app){
+    //cookieparser is necessary for reading the user data
+    app.use(cookieparser());
 //Get the chores for a parent in that household
 app.get("/api/household", function(req, res) {
 
@@ -132,38 +136,79 @@ app.get("/api/household/child", function(req, res) {
             });
         });
 
-    //Delete a chore
-
-
 //post route which connects to the login button.
-router.post('/token', function(req,res) {
+app.post('/login', function (req, res) {
     //Normally, you would fetch the user from the database here.
     //then save the relevant info in user details
-    const userDetails = {
-        username: TEST_USER.username,
-        id: TEST_USER.id,
-        foo: TEST_USER.foo,
-    };
+    db.person.findOne({
+        where: {
+            userID: req.body.Username,
+        },
+    }).then((function (userObj) {
+        //test for userObj being found
+        if (!userObj) { return res.sendStatus(404) }
 
 
-    // do some sort of check that the user/password is correct:
-    //normally, you would do this with the user in the database. 
-    //in this case, we just have a "test user" saved in a file somewhere
-    if (req.body.username === TEST_USER.username && req.body.password === TEST_USER.password) {
 
-        jwt.sign(userDetails, JWT_SECRET_KEY, JWT_OPTIONS, 
-        function(err, token) {
-            if (err) return res.sendStatus(500).json(err) //do some error checking
-            res.json({
-                user: userDetails,
-                token: token,
-            })
+        else if (userObj.password === req.body.Password) {
+            console.log("succesful login")
+            user = {
+                Username: userObj.userID,
+                Password: userObj.password,
+                houseId: userObj.houseId,
+                personId: userObj.personId,
+                isParent: userObj.isParent
+            }
 
+            jwt.sign(user, JWT_SECRET_KEY, JWT_OPTIONS,
+                function (err, token) {
+                    if (err) return res.sendStatus(500).json(err) //do some error checking
+                    else {
+                        res.json({
+                            user,
+                            Token: token,
+                        })
+                    }
+
+                })
+        }
+        else {
+            res.sendStatus(401).send();
+        }
+    })).catch(function(err){
+        return res.sendStatus(500).json(err) 
+    });
+})
+
+
+}
+let verifyToken = function (req, res, next) {
+    //declare the token from each request. 
+    //The token is sent automatically in a cookie,
+    // cookie_parser allows us to read the cookie easily
+    const token = req.cookies.Token;
+    //first determine if a token is found
+    if (token) {
+        //with this nested if I verify the token. 
+        //the callback in jwt.verify will return true if verified w/ jwt's verified.
+        //the contents of the if statement are actually just the next() function
+     if (jwt.verify(token, JWT_SECRET_KEY, function (err, data) {
+         //self explanatory: not verified => forbidden.
+         //interestingly, this does succesfully end the verifyToken function as if it was returned. 
+            if (err) { res.sendStatus(403) }
+            //return true to fulfill the if, token verified for testing
+            else {
+                console.log("token verified")
+                return true
+            }
         })
-
+    ); {
+        //next() is what makes the middleware function continue to the next function
+        next();
     }
-    else {
-        res.sendStatus(401).send();
+} else {
+    //403 means forbidden, slightly stronger than unauthorized.
+    //this is else if the token is not found in cookies. 
+    res.sendStatus(403);
     }
-});
 }
