9812385ef4709abe45566e1a31ccafcce64e0409
Taylor <UkrainianEagle@q.com>
2019-03-14 13:37:49 -0500

    Added houseid and personid cookies in authentication_merge.js and removed verifyToken from /register

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/assets/html/home.html"));
   });
 
-  app.get("/register", verifyToken, function (req, res) {
+  app.get("/register", function (req, res) {
     console.log(req.body);
     res.sendFile(path.join(__dirname, "../public/assets/html/register.html"));
   });
